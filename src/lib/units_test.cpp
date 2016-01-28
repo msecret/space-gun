ef58eb64d781a339525098eed56ced270332909f
msecret <msegreto@miceover.com>
2016-01-28 15:57:11 -0800

    Finish up units tests

--- src/lib/units_test.cpp
+++ src/lib/units_test.cpp
@@ -7,3 +7,11 @@ TEST(Units, DefaultConstructor) {
   EXPECT_EQ(0, va.x);
   EXPECT_EQ(0, va.y);
 }
+
+TEST(Units, AdditionOperator) {
+  const aronnax::Vector2d va(5, 3);
+  const aronnax::Vector2d vb(1, 3);
+  aronnax::Vector2d vc = va + vb;
+  EXPECT_EQ(6, vc.x);
+  EXPECT_EQ(6, vc.y);
+}
