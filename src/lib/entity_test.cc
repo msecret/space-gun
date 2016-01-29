30a93bd0881c73eedc979c6c71bde6f0e6e9bac9
msecret <msegreto@miceover.com>
2016-01-28 18:08:10 -0800

    Ensure all pointers are torn down
    This ensures to avoid leaks.
    
    This could also be down with unique_ptrs. I did it this way only
    because it's a little easier to create a few ptrs this way rather
    then creating std ones.

--- src/lib/entity_test.cc
+++ src/lib/entity_test.cc
@@ -3,8 +3,7 @@
 #include "entity.h"
 #include "gtest/gtest.h"
 
-/*
-class TestC : public aronnax::Component {
+class TestComponent : public aronnax::Component {
   public:
     void update(aronnax::Entity &entity, const uint32_t dt)
     {
@@ -16,7 +15,6 @@ class TestC : public aronnax::Component {
       return "TestComponent";
     }
 };
-*/
 
 class TestRenderer : public aronnax::Renderer {
   public:
@@ -32,11 +30,18 @@ protected:
 
   virtual void SetUp() {
     ea_ = new aronnax::Entity(cla_);
+    ca_ = new TestComponent();
+  }
+
+  virtual void TearDown() {
+    delete ea_;
+    delete ca_;
   }
 
   // Declares the variables your tests want to use.
   aronnax::Components cla_;
   aronnax::Entity* ea_;
+  TestComponent* ca_;
 };
 
 TEST_F(EntityTest, DefaultConstructor) {
@@ -50,6 +55,22 @@ TEST_F(EntityTest, DefaultConstructor) {
 
 TEST_F(EntityTest, RendererConstructor) {
   std::shared_ptr<TestRenderer> testRenderer = std::make_shared<TestRenderer>();
-  aronnax::Entity* ea = new aronnax::Entity(cla_, testRenderer);
-  EXPECT_EQ(ea->getRenderer(), testRenderer.get());
+  ea_ = new aronnax::Entity(cla_, testRenderer);
+  EXPECT_EQ(ea_->getRenderer(), testRenderer.get());
+}
+
+TEST_F(EntityTest, hasComponent) {
+  cla_.push_back(ca_);
+  ea_ = new aronnax::Entity(cla_);
+  bool actual = ea_->hasComponent("TestComponent");
+  EXPECT_TRUE(actual);
+  actual = ea_->hasComponent("NoComponent");
+  EXPECT_FALSE(actual);
+}
+
+TEST_F(EntityTest, getComponent) {
+  cla_.push_back(ca_);
+  ea_ = new aronnax::Entity(cla_);
+  auto actual = ea_->getComponent("TestComponent");
+  EXPECT_EQ(actual, ca_);
 }
