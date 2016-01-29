9e493b71046b1a2760c1e34dbd7835e643435d24
msecret <msegreto@miceover.com>
2016-01-28 16:02:08 -0800

    Change entity test to use fixtures
    This allows me to reuse thes same data structures in teh tests.

--- src/lib/entity_test.cc
+++ src/lib/entity_test.cc
@@ -16,26 +16,25 @@ class TestC : public aronnax::Component {
       return "TestComponent";
     }
 };
+*/
 
 class EntityTest : public testing::Test {
 protected:
 
   virtual void SetUp() {
+    ea_ = new aronnax::Entity(cla_);
   }
 
   // Declares the variables your tests want to use.
   aronnax::Components cla_;
-  aronnax::Entity ea_;
+  aronnax::Entity* ea_;
 };
-*/
 
-TEST(Entity, DefaultConstructor) {
-  const aronnax::Components cla_;
-  const aronnax::Entity ea_(cla_);
-  EXPECT_EQ(0, ea_.pos.x) << "Constructs with position as 0 by default";
-  EXPECT_EQ(0, ea_.pos.y);
-  EXPECT_EQ(0, ea_.box.x);
-  EXPECT_EQ(0, ea_.box.y);
-  EXPECT_EQ(0, ea_.v.x);
-  EXPECT_EQ(0, ea_.v.y);
+TEST_F(EntityTest, DefaultConstructor) {
+  EXPECT_EQ(0, ea_->pos.x) << "Constructs with position as 0 by default";
+  EXPECT_EQ(0, ea_->pos.y);
+  EXPECT_EQ(0, ea_->box.x);
+  EXPECT_EQ(0, ea_->box.y);
+  EXPECT_EQ(0, ea_->v.x);
+  EXPECT_EQ(0, ea_->v.y);
 }
