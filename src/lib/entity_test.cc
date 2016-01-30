2516d60c51bebc1b766ed43aec97cec528aa1d73
msecret <msegreto@miceover.com>
2016-01-29 18:32:46 -0800

    Finished up entity tests
    Moved renderer constructor test to get renderer and tested the
    render function.

--- src/lib/entity_test.cc
+++ src/lib/entity_test.cc
@@ -65,11 +65,6 @@ TEST_F(EntityTest, DefaultConstructor) {
   EXPECT_EQ(0, ea_->v.y);
 }
 
-TEST_F(EntityTest, RendererConstructor) {
-  std::shared_ptr<TestRenderer> testRenderer = std::make_shared<TestRenderer>();
-  ea_ = new aronnax::Entity(cla_, testRenderer);
-  EXPECT_EQ(ea_->getRenderer(), testRenderer.get());
-}
 
 TEST_F(EntityTest, hasComponent) {
   cla_.push_back(ca_);
@@ -78,6 +73,12 @@ TEST_F(EntityTest, hasComponent) {
   EXPECT_TRUE(actual);
   actual = ea_->hasComponent("NoComponent");
   EXPECT_FALSE(actual);
+}
+
+TEST_F(EntityTest, getRenderer) {
+  std::shared_ptr<TestRenderer> testRenderer = std::make_shared<TestRenderer>();
+  ea_ = new aronnax::Entity(cla_, testRenderer);
+  EXPECT_EQ(ea_->getRenderer(), testRenderer.get());
 }
 
 TEST_F(EntityTest, getComponent) {
@@ -101,4 +102,16 @@ TEST_F(EntityTest, update) {
   EXPECT_CALL(mockComponent, update(_, testDt)).Times(1);
 
   ea_->update(testDt);
+}
+
+TEST_F(EntityTest, render) {
+  const uint32_t testDt = 1;
+  NiceMock<MockComponent> mockComponent;
+
+  cla_.push_back(&mockComponent);
+  ea_ = new aronnax::Entity(cla_);
+
+  EXPECT_CALL(mockComponent, render(_)).Times(1);
+
+  ea_->render();
 }
