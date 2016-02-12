35623f695a296498856b950697c7c5164e9a9de0
msecret <msegreto@miceover.com>
2016-02-11 23:11:08 -0800

    Add operators to units
    To easily add to existing and compoare vector 2ds.

--- src/lib/units_test.cpp
+++ src/lib/units_test.cpp
@@ -15,3 +15,19 @@ TEST(Units, AdditionOperator) {
   EXPECT_EQ(6, vc.x);
   EXPECT_EQ(6, vc.y);
 }
+
+TEST(Units, AdditionAssignmentOperator) {
+  aronnax::Vector2d va(5, 3);
+  aronnax::Vector2d vb(1, 3);
+
+  va += vb;
+  EXPECT_EQ(6, va.x);
+  EXPECT_EQ(6, va.y);
+}
+
+TEST(Units, EqualityOperator) {
+  aronnax::Vector2d va(2, 3);
+  aronnax::Vector2d vb(2, 3);
+
+  EXPECT_EQ(va, vb);
+}
