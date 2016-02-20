c853f387bf9f306d61e1e8b95210a68a5b8689ab
Marco Segreto <marco.segreto@gsa.gov>
2016-02-20 10:54:46 -0600

    Add color struct
    This will add a layer of separation from SDL colors. This is done so
    if the library implementation changes to something other then SDL, the
    colors will not have to change.
    
    One potential problem I see with this is that alpha is an uint8 rather
    then a float, which may not be the most common implementation.

--- src/lib/units.h
+++ src/lib/units.h
@@ -1,56 +1,83 @@
 
+#include <cstdint>
+
 #ifndef _h_Units
 #define _h_Units
 
 namespace aronnax {
 
-struct Vector2d
-{
-  double x;
-  double y;
-
-  Vector2d(const double x=0, const double y=0): x(x), y(y)
+  struct Vector2d
   {
-  }
+    double x;
+    double y;
 
-  Vector2d operator+(const Vector2d& a) const
+    Vector2d(const double x=0, const double y=0): x(x), y(y)
+    {
+    }
+
+    Vector2d operator+(const Vector2d& a) const
+    {
+      return Vector2d(a.x + x, a.y + y);
+    }
+
+    Vector2d operator-(const Vector2d& a) const
+    {
+      return Vector2d(x - a.x, y - a.y);
+    }
+
+    Vector2d operator*(const Vector2d& a) const
+    {
+      return Vector2d(a.x * x, a.y * y);
+    }
+
+    Vector2d operator/(const Vector2d& a) const
+    {
+      return Vector2d(x / a.x, y / a.y);
+    }
+
+    Vector2d& operator+=(const Vector2d& v) 
+    { 
+      x += v.x;
+      y += v.y; 
+
+      return *this; 
+    }
+
+    bool operator==(const Vector2d& a) const
+    {
+      return a.x == x && a.y == y;
+    }
+
+    bool operator!=(const Vector2d& a) const
+    {
+      return x != a.x && y != a.y;
+    }
+  };
+
+  struct Color 
   {
-    return Vector2d(a.x + x, a.y + y);
-  }
+    uint8_t r, g, b, a; 
 
-  Vector2d operator-(const Vector2d& a) const
-  {
-    return Vector2d(x - a.x, y - a.y);
-  }
+    Color(const uint8_t red=0,
+          const uint8_t green=0,
+          const uint8_t blue=0,
+          const uint8_t alpha=0) :
+      r(red),
+      g(green),
+      b(blue),
+      a(alpha)
+    { }
 
-  Vector2d operator*(const Vector2d& a) const
-  {
-    return Vector2d(a.x * x, a.y * y);
-  }
+    bool operator==(const Color& c) const
+    {
+      return r == c.r && g == c.g && b == c.b && a == c.a;
+    }
 
-  Vector2d operator/(const Vector2d& a) const
-  {
-    return Vector2d(x / a.x, y / a.y);
-  }
-
-  Vector2d& operator+=(const Vector2d& v) 
-  { 
-    x += v.x;
-    y += v.y; 
-
-    return *this; 
-  }
-
-  bool operator==(const Vector2d& a) const
-  {
-    return a.x == x && a.y == y;
-  }
-
-  bool operator!=(const Vector2d& a) const
-  {
-    return x != a.x && y != a.y;
-  }
-};
+    bool operator!=(const Color& c) const
+    {
+      return r != c.r || g != c.g || b != c.b || a != c.a;
+    }
+  };
 
 }
 
--- src/lib/units_test.cpp
+++ src/lib/units_test.cpp
@@ -2,13 +2,13 @@
 #include "units.h"
 #include "gtest/gtest.h"
 
-TEST(Units, DefaultConstructor) {
+TEST(VectorUnits, DefaultConstructor) {
   const aronnax::Vector2d va;
   EXPECT_EQ(0, va.x);
   EXPECT_EQ(0, va.y);
 }
 
-TEST(Units, AdditionOperator) {
+TEST(VectorUnits, AdditionOperator) {
   const aronnax::Vector2d va(5, 3);
   const aronnax::Vector2d vb(1, 3);
   aronnax::Vector2d vc = va + vb;
@@ -16,7 +16,7 @@ TEST(Units, AdditionOperator) {
   EXPECT_EQ(6, vc.y);
 }
 
-TEST(Units, SubtractionOperator) {
+TEST(VectorUnits, SubtractionOperator) {
   aronnax::Vector2d va(1, 2);
   aronnax::Vector2d vb(1, 1);
   aronnax::Vector2d expected(0, 1);
@@ -26,7 +26,7 @@ TEST(Units, SubtractionOperator) {
   EXPECT_EQ(expected.y, actual.y);
 }
 
-TEST(Units, MultiplicationOperator) {
+TEST(VectorUnits, MultiplicationOperator) {
   aronnax::Vector2d va(1, 2);
   aronnax::Vector2d vb(3, 2);
   aronnax::Vector2d expected(3, 4);
@@ -36,7 +36,7 @@ TEST(Units, MultiplicationOperator) {
   EXPECT_EQ(expected.y, actual.y);
 }
 
-TEST(Units, DivisionOperator) {
+TEST(VectorUnits, DivisionOperator) {
   aronnax::Vector2d va(10, 6);
   aronnax::Vector2d vb(2, 3);
   aronnax::Vector2d expected(5, 2);
@@ -46,7 +46,7 @@ TEST(Units, DivisionOperator) {
   EXPECT_EQ(expected.y, actual.y);
 }
 
-TEST(Units, AdditionAssignmentOperator) {
+TEST(VectorUnits, AdditionAssignmentOperator) {
   aronnax::Vector2d va(5, 3);
   aronnax::Vector2d vb(1, 3);
 
@@ -55,10 +55,33 @@ TEST(Units, AdditionAssignmentOperator) {
   EXPECT_EQ(6, va.y);
 }
 
-TEST(Units, EqualityOperator) {
+TEST(VectorUnits, EqualityOperator) {
   aronnax::Vector2d va(2, 3);
   aronnax::Vector2d vb(2, 3);
 
   EXPECT_EQ(va, vb);
 }
 
+
+TEST(ColorUnits, Constructor) {
+  aronnax::Color ca(255, 255, 200, 0);
+
+  EXPECT_EQ(255, ca.r);
+  EXPECT_EQ(255, ca.g);
+  EXPECT_EQ(200, ca.b);
+  EXPECT_EQ(0, ca.a);
+}
+
+TEST(ColorUnits, EqualityOperator) {
+  aronnax::Color ca(255, 255, 200, 0);
+  aronnax::Color cb(255, 255, 200, 0);
+
+  EXPECT_EQ(ca, cb);
+}
+
+TEST(ColorUnits, InequalityOperator) {
+  aronnax::Color ca(255, 255, 200, 0);
+  aronnax::Color cb(255, 110, 200, 0);
+
+  EXPECT_NE(ca, cb);
+}
